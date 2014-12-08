9f855db999899d97eaae38e35b02e009b32225ca
Michael <michael@dubspeed.de>
2014-12-08 18:24:23 +0100

    added filter-none that simply copies the image

---
+++ filters/filter-none.js
@@ -0,0 +1,81 @@
+/* jshint: node=true */
+
+(function() {
+    'use strict';
+
+    var _ = require('lodash');
+    var Promise = require('bluebird');
+    var temp = require('temp').track();
+    var exec = Promise.promisifyAll( require('child_process') );
+    var fs = require( 'fs' );
+
+    // keep track of all the pathes we request
+    var temp_path = [];
+
+    /**
+     * Inspect the input data and may return usefull
+     * interpreation of them
+     */
+    exports.inspect = function( data ) {
+        var stats;
+        if ( fs.existsSync( data.filename ) ) {
+            stats = fs.statSync( data.filename );
+        }
+
+        data.size = stats.size;
+        data.filter = 'none';
+        return data;
+
+    };
+
+    exports.filter = function ( filename, options ) {
+        options = options || {};
+
+        _.defaults( options, {
+            parameters: [],
+            resultFilename : temp.path( { suffix : '.png' } )
+        } );
+
+        temp_path.push( options.resultFilename );
+
+        console.log('filter-none: returning original image' );
+
+        //var cmd = [].concat( options.parameters, [ filename, options.resultFilename ] );
+
+        var promise = new Promise( function( resolve, reject ) {
+            var in_file = fs.createReadStream( filename );
+            var out_file = fs.createWriteStream( options.resultFilename );
+            in_file.on( 'error', reject );
+            out_file.on( 'error', reject );
+            in_file.pipe( out_file );
+            resolve( [ '', '' ]);
+        } );
+
+        promise = promise.then( function( output ) {
+            return {
+                filename : options.resultFilename,
+                output : {
+                    stderr : output[ 0 ],
+                    stdout : output[ 1 ]
+                },
+                parameters : options.parameters
+            };
+        } );
+
+        return promise;
+    };
+
+    exports.cleanup = function() {
+        console.log('filter-none: cleaning up');
+        temp.cleanup();
+
+        // we used temp.path, this is not tracked by temp
+        temp_path.forEach( function( path ) {
+            if ( fs.existsSync( path ) ) {
+                fs.unlinkSync( path );
+            }
+        } );
+        temp_path = [];
+    };
+
+}());
